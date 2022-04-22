1e303ed685233036e17be89e489fbf5d41054f19
noranbear <norandoly@gmail.com>
2022-04-22 23:49:17 +0900

    App for getting Customer info

--- day10/src/p344/App.java
+++ day10/src/p344/App.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- *
+ * 유저에게 정보를 받아서 CustomerVO에 담은 다음, OracleDAO에 보낸다.
  * @author noranbear (norandoly@gmail.com)
  * @since 2022. 4. 20. 오전 11:22:14
  */
@@ -15,14 +15,19 @@ public class App {
 
 	public static void main(String[] args) {
 		System.out.println("Start...");
-		OracleDAO dao = new OracleDAO();	// App -> OracleDAO (App은 OracleDAO가 필요하다
-											// DirectedAssociation 관계
+		
+		// OracleDAO 객체를 만든다.
+		// *App과 OracleDAO를 이어주는 부분
+		OracleDAO dao = new OracleDAO();	
+		
 		Scanner sc = new Scanner(System.in);
 		
 		while(true) {
+			// 메뉴
 			System.out.println("Input cmd(i,d,s,a,q): ");		
 			String cmd = sc.next();							
 			
+			// q: 메뉴에서 나간다.
 			if(cmd.equals("q")) {				
 				System.out.println("Quit");
 				break;
@@ -43,14 +48,14 @@ public class App {
 				// 정보를 보냄.
 				dao.insert(c);
 			
-			// d: 정보를 지운다.
+			// d: 정보를 지운다. (지웠다고 얘기해준다)
 			}else if(cmd.equals("d")) {
 				System.out.println("Input Customer id: ");
 				String id = sc.next();
 				
 				dao.delete(id);
 			
-			// s: 정보를 불러낸다.
+			// s: 정보를 불러낸다. (가상의 정보를 보내준다)
 			}else if(cmd.equals("s")) {
 				System.out.println("Input Customer id: ");
 				String id = sc.next();
@@ -58,7 +63,7 @@ public class App {
 				CustomerVO c = dao.select(id);
 				System.out.println(c);
 				
-			// a: DB에 있는 모든 정보를 불러낸다.
+			// a: DB에 있는 모든 정보를 불러낸다. (모든 가상의 정보를 보내준다)
 			}else if(cmd.equals("a")) {
 				ArrayList<CustomerVO> a = dao.select();
 				
