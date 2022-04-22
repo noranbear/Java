a392ebeea3988b60ea71a5f48f7964b744554a9f
noranbear <norandoly@gmail.com>
2022-04-23 02:13:42 +0900

    DAO Interface

--- day10/src/p345/DAO.java
+++ day10/src/p345/DAO.java
@@ -6,13 +6,14 @@ package p345;
 import java.util.ArrayList;
 
 /**
- *
+ * OracleDAO와 MariadbDAO의 기능이 정의되어 있는 인터페이스
  * @author noranbear (norandoly@gmail.com)
  * @since 2022. 4. 20. 오후 2:09:59
  */
 public interface DAO {
 	
-	// 인터페이스는 일반 함수도 정의할 수 있다 - default 키워드 사용
+	// 인터페이스는 하위 클래스에서 공통적으로 쓰일 일반 함수도 정의할 수 있다.
+	// default 키워드 사용
 	/**
 	 * 가상의 서버 접속 메소드
 	 */
@@ -26,7 +27,8 @@ public interface DAO {
 		System.out.println("Oracle Server Closed........");
 	}
 	
-	public void insert(CustomerVO c);		// 인터페이스에 이렇게 쓰면 자동 추상메소드화
+	// 인터페이스에 Abstract keyword 없이 method header만 써도 추상메소드화함.
+	public void insert(CustomerVO c);		
 	public void delete(String id);
 	public CustomerVO select(String id);
 	public ArrayList<CustomerVO> select();
