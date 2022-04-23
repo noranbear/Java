4162dd280df1a6ba55c9e497f09e05711849d29f
noranbear <norandoly@gmail.com>
2022-04-24 02:46:22 +0900

    Electric Car inherits Car (배터리가 달기 전에 간 거리 계산)

--- day09/src/simCar/Electro.java
+++ day09/src/simCar/Electro.java
@@ -60,60 +60,123 @@ public class Electro extends Car {
 		return batteryE;
 	}
 
-	
+	/**
+	 * (배터리가 있는 만큼)앞으로 간다.
+	 */
 	@Override
 	public void go(int distance) {
+		// 간 거리
+		double dWent = 0.0;
+		// 원래 배터리양
+		double dBattery = 0.0;
+		dBattery = cBattery;
+		
 		// 1. 연비계산을 한다. (가솔린차의 연비를 그냥 따랐다)
 		cBattery -= distance/batteryE;
 		
-		// 배터리가 없는 경우
-		if(cBattery <= 0) {
-			if(getStatus().equals("GO") || getStatus().equals("BACK")) {
-				System.out.println("0% of battery. This car would stop soon. 1, 2, 3...\n");
-				setStatus("STOP");
-				
-			}else {
-				System.out.println("0% of battery. This car can't move.\n");
-			}
-		// 배터리가 부족한 경우		
-		}else if(cBattery <= 5) {
-			setStatus("GO");
-			System.out.printf("Go %dkm.\n",distance);
-			System.out.println("Warning! Low percentage of battery.\n");
+		// 2.1 연비계산 후 배터리가 없는 경우
+		if(cBattery < 0) {
+			// 2.1.1 현재 배터리양이 마이너스값이 되는 것을 막는다.
+			cBattery = 0;
 			
-		}else {
+			// 2.1.2 남은 distance를 계산한다.
+			dWent = batteryE * dBattery;	// 간 거리
+			distance -= dWent;				// 남은 거리
+			
+			// 3.1 차가 움직이고 있는 경우
+			if(getStatus().equals("GO") || getStatus().equals("BACK")) {
+				// 3.1.1 남은 배터리로 갈 수 있을 만큼 간다.
+				System.out.printf("This car went for %.2fkm.\n",dWent);
+				// 3.1.2 배터리가 다 달면 멈춘다.
+				System.out.println("0% of battery. This car would stop soon. "
+									+ "1, 2, 3...\n");
+				setStatus("STOP");
+			
+			// 3.2 차가 멈춰있던 경우	
+			}else {
+				// 3.2.1 남은 배터리로 갈 수 있는 만큼 간다.
+				System.out.printf("This car went for %.2fkm.\n",dWent);
+				setStatus("GO");
+				// 3.2.2 배터리가 다 달면 멈춘다.
+				System.out.println("0% of battery. This car can't move.\n");
+				setStatus("STOP");
+			}
+			
+		// 2.2 배터리가 5%이하가 되는 경우		
+		}else if(cBattery <= 5) {
+			// 2.2.1 앞으로 간다.
 			setStatus("GO");
-			System.out.printf("Go %dkm.\n",distance);
+			System.out.printf("Go for %dkm.\n",distance);
+			// 2.2.2 경고를 내보낸다.
+			System.out.println("Warning! Low percentage of battery.\n");
+		
+		// 2.3 배터리가 충분한 경우	
+		}else {
+			// 2.3.1 앞으로 간다.
+			setStatus("GO");
+			System.out.printf("Go for %dkm.\n",distance);
 		}
 	}
 
 	@Override
 	public void back(int distance) {
-		cBattery -= distance/batteryE;
-		
-		// 배터리가 없는 경우
-		if(cBattery <= 0) {
-			if(getStatus().equals("GO") || getStatus().equals("BACK")) {
-				System.out.println("0% of battery. This car would stop soon. 1, 2, 3...\n");
-				setStatus("STOP");
+		// 간 거리
+		double dWent = 0.0;
+		// 원래 배터리양
+		double dBattery = 0.0;
 				
+		dBattery = cBattery;
+				
+		// 1. 연비계산을 한다. (가솔린차의 연비를 그냥 따랐다)
+		cBattery -= distance/batteryE;
+				
+		// 2.1 연비계산 후 배터리가 없는 경우
+		if(cBattery < 0) {
+			// 2.1.1 현재 배터리양이 마이너스값이 되는 것을 막는다.
+			cBattery = 0;
+					
+			// 2.1.2 남은 distance를 계산한다.
+			dWent = batteryE * dBattery;	// 간 거리
+			distance -= dWent;				// 남은 거리
+					
+			// 3.1 차가 움직이고 있는 경우
+			if(getStatus().equals("GO") || getStatus().equals("BACK")) {
+				// 3.1.1 남은 배터리로 갈 수 있을 만큼 간다.
+				System.out.printf("This car wentward back for %.2fkm.\n",dWent);
+				// 3.1.2 배터리가 다 달면 멈춘다.
+				System.out.println("0% of battery. This car would stop soon. "
+									+ "1, 2, 3...\n");
+				setStatus("STOP");
+					
+			// 3.2 차가 멈춰있던 경우	
 			}else {
+				// 3.2.1 남은 배터리로 갈 수 있는 만큼 간다.
+				System.out.printf("This car went backward for %.2fkm.\n",dWent);
+				setStatus("BACK");
+				// 3.2.2 배터리가 다 달면 멈춘다.
 				System.out.println("0% of battery. This car can't move.\n");
+				setStatus("STOP");
 			}
-		// 배터리가 부족한 경우		
+					
+		// 2.2 배터리가 5%이하가 되는 경우		
 		}else if(cBattery <= 5) {
+			// 2.2.1 뒤로 간다.
 			setStatus("BACK");
-			System.out.printf("Back %dkm.\n",distance);
+			System.out.printf("Go backward for %dkm.\n",distance);
+			// 2.2.2 경고를 내보낸다.
 			System.out.println("Warning! Low percentage of battery.\n");
-			
+				
+		// 2.3 배터리가 충분한 경우	
 		}else {
+			// 2.3.1 뒤로 간다.
 			setStatus("BACK");
-			System.out.printf("Back %dkm.\n",distance);
+			System.out.printf("Go backward for %dkm.\n",distance);
 		}
 	}
-
+	
+	
 	public void chargeBattery(int per) {
-		//* total size보다 크면 
+		// 충전한 양이 총 배터리의 양을 넘을 경우
 		if((cBattery += per) >= batterySize) {
 			System.out.println("Battery is fully charged.");
 			cBattery = 100;
