6855d539ea57dccf6b41c17e53a959a4ed00f158
noranbear <norandoly@gmail.com>
2022-04-23 18:59:48 +0900

    Search Interface

---
+++ day10/src/ws/Search.java
@@ -0,0 +1,12 @@
+package ws;
+
+import java.util.ArrayList;
+
+/**
+ * Search Interface
+ * @author noranbear (norandoly@gmail.com)
+ * @since 2022. 4. 21. ¿ÀÀü 10:14:10
+ */
+public interface Search {
+	public ArrayList<TodoVO> search(boolean done);
+}
