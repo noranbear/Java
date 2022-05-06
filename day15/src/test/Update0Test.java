c7d63492cce9e967413d38022a6b099601272f45
noranbear <norandoly@gmail.com>
2022-05-07 01:11:03 +0900

    WS Library JDBC

--- day15/src/test/Update0Test.java
+++ day15/src/test/Update0Test.java
@@ -8,7 +8,7 @@ import vo.BookVo;
  * update0() Test
  * @author noranbear (norandoly@gmail.com)
  * @since 2022. 4. 27. ¿ÀÈÄ 2:39:29
- * @version 1.0
+ * @version 2.0
  */
 public class Update0Test {
 
@@ -17,9 +17,10 @@ public class Update0Test {
 		
 		try {
 			dao.update0(4);
+			System.out.println("Completed");
 		} catch (Exception e) {
 			
-			e.printStackTrace();
+			System.out.println(e.getMessage());
 		}
 	}
 
