fefe063996bcfe45ace4be331f147e7a718d51b6
noranbear <norandoly@gmail.com>
2022-05-06 16:20:51 +0900

    Librarydb App

---
+++ day15/src/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+/**
+ * UI class를 실행하는 Application
+ * @author noranbear (norandoly@gmail.com)
+ * @since 2022. 4. 27. 오후 3:29:07
+ * @version 1.0
+ */
+public class App {
+
+	public static void main(String[] args) {
+		// UI 실행
+		UI ui = new UI();
+	}
+
+}
